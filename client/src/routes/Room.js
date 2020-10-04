ed029c8b417ecc2c1080fafeef58ec7bd4179c40
Ayush Shukla <aks18041999@gmail.com>
2020-10-05 00:11:38 +0530

    SERVER NAME

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -74,13 +74,11 @@ const Room = (props) => {
   function createPeer(userID) {
     const peer = new RTCPeerConnection({
       iceServers: [
+        { url: "stun:stun.l.google.com:19302" },
         {
-          urls: "stun:stun.stunprotocol.org",
-        },
-        {
-          urls: "turn:numb.viagenie.ca",
-          credential: "muazkh",
-          username: "webrtc@live.com",
+          url: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
         },
       ],
     });
